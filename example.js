e89eec669b635c812105bd59eea3709f89cec462
charlie wolf <charlie@wolf.is>
2015-02-24 11:28:28 -0800

    clean up example

--- example.js
+++ example.js
@@ -1,23 +1,41 @@
 var util = require('util');
-var assert = require('assert');
 var cacheManager = require('cache-manager');
 var memcachedStore = require('./index');
-var memcachedCache = cacheManager.caching({store: memcachedStore, servers: ['127.0.0.1:11211'], ttl: 100});
+var memcachedCache = cacheManager.caching({
+    store: memcachedStore,
+    servers: ['127.0.0.1:11211'],
+    ttl: 100
+});
 var ttl = 60;
 
 console.log("set/get/del example:");
 
-memcachedCache.set('foo', 'bar', {ttl: ttl}, function(err) {
-    if (err) { throw err; }
+memcachedCache.set('foo', 'bar', {
+    ttl: ttl
+}, function(err) {
+    if (err) {
+        throw err;
+    }
 
     memcachedCache.get('foo', function(err, result) {
-        if (err) { throw err; }
+        if (err) {
+            throw err;
+        }
         console.log("result fetched from cache: " + result);
-            memcachedCache.del('foo', function(err) {
-                if (err) { throw err; }
-		console.log('deleted');
-		process.exit();
+        memcachedCache.del('foo', function(err) {
+            if (err) {
+                throw err;
+            }
+            console.log('deleted');
+            memcachedCache.get('foo', function(err, result) {
+                if (err) {
+                    throw err;
+                }
+                console.log('now it should be gone');
+                console.log(result);
+                process.exit();
             });
+        });
 
     });
 });
